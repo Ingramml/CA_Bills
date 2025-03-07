b562368f4a22a821c4224f4ac6350710ec70b775
Ingramml <Ingramml@gmail.com>
2025-03-07 10:21:52 -0500

    major_update

--- Cabill_builder_xml finds.py
+++ Cabill_builder_xml finds.py
@@ -1,84 +1,74 @@
 import os
 import xml.etree.ElementTree as ET
 import pandas as pd
+from datetime import date
+import glob
 
+today=str(date.today())
 
-
-input_path='/Users/michaelingram/Downloads/pubinfo_daily_Wed/BILL_VERSION_TBL_628.lob'
+input_path='/Users/michaelingram/Downloads/pubinfo_daily_Wed (2)'
 ns = {  'caml':'http://lc.ca.gov/legalservices/schemas/caml.1#',
         'xlink':'http://www.w3.org/1999/xlink',
        ' xhtml':'http://www.w3.org/1999/xhtml',
         'xsi':'http://www.w3.org/2001/XMLSchema-instance'}
-bill_data = []
+bill_data = {}
 
     # Determine if input is a folder or a single file
 files_to_process = []
 if os.path.isdir(input_path):
         files_to_process = [
             os.path.join(input_path, f) for f in os.listdir(input_path) 
-            if f.endswith(".xml") or f.endswith(".lob")
+            if f.endswith(".xml") or f.endswith(".lob") and f.startswith("BILL_VERSION_TBL_")
         ]
 elif os.path.isfile(input_path) and (input_path.endswith(".xml") or input_path.endswith(".lob")):
         files_to_process = [input_path]
 else:
     print("Invalid input. Please provide a valid XML file or folder containing XML files.")
 
-
+bill_data=[]
     # Process each file
 for file_path in files_to_process:
     filename = os.path.basename(file_path)
+    print(filename)
+    print(file_path)
     tree = ET.parse(file_path)
     root = tree.getroot()
-    #print(root[0][0].text)
-"""
-for child in root:
-    print(child.tag, child.attrib)
-    with open('output.txt', 'w') as f:
-        print(root.attrib, file=f)
-"""    
-#print(root[0][2].tag)
+    
+    base=root[0]
+    namespace='{http://lc.ca.gov/legalservices/schemas/caml.1#}'
+    Id=base.find(namespace+'Id').text.replace("__", "")
+    #print('ID:'+Id)
+    History=base.find(namespace+'History')
+    #print(History)
+    Actiontext =base.find('.//'+namespace+'ActionText')
+    #print('Action Text:'+Actiontext.text)
+    Actiondate=base.find('.//'+namespace+'ActionDate')
+    #print('Action Date:'+Actiondate.text)
 
-base=root[0]
-namespace='{http://lc.ca.gov/legalservices/schemas/caml.1#}'
-Id=base.find(namespace+'Id')
-print('ID:'+Id.text)
-History=base.find(namespace+'History')
-print(History.text)
-Actiontext =base.find('.//'+namespace+'ActionText')
-print('Action Text:'+Actiontext.text)
-Actiondate=base.find('.//'+namespace+'ActionDate')
-print('Action Date:'+Actiondate.text)
+    title = base.find("caml:Title", ns).text if base.find("caml:Title", ns) is not None else None
+    #print('Title:'+title)
 
-title = base.find("caml:Title", ns).text if base.find("caml:Title", ns) is not None else None
-print('Title:'+title)
+    session_year = base.find('.//'+namespace+'SessionYear', ns).text if root[0].find('.//'+namespace+'SessionYear', ns) is not None else None
+    #print("Session Year "+ str(session_year))
 
-session_year = base.find(".//caml:SessionYear", ns).text if root[0].find("caml:SessionYear", ns) is not None else None
-print(session_year)
+    session_number =  base.find('.//'+namespace+'SessionNum', ns).text if root[0].find('.//'+namespace+'SessionNum', ns) is not None else None
+    #print('Session Number:'+session_number)
 
-session_number =  base.find(".//caml:SessionNum", ns).text if root[0].find("caml:SessionNum", ns) is not None else None
-print('Session Number:'+session_number)
-
-
-#Description=root.find('http://lc.ca.gov/legalservices/schemas/caml.1#}Description')
-#print(Description)
-
-"""
-    # Extract relevant fields
-    for bill in root.findall("caml:Bill", ns):
-        bill_id = bill.get("id")  # Attribute extraction
-        title = bill.find("caml:Title", ns).text if bill.find("caml:Title", ns) is not None else None
-        session_year = root.find("caml:SessionYear", ns).text if root.find("caml:SessionYear", ns) is not None else None
-        measure_type = root.find("caml:MeasureType", ns).text if root.find("caml:MeasureType", ns) is not None else None
-        subject = root.find("caml:Subject", ns).text if root.find("caml:Subject", ns) is not None else None
-
-        bill_data.append({
-            "Bill ID": bill_id,
+    digesttext =  base.find('./'+namespace+'DigestText/*').text if base.find('./'+namespace+'DigestText/*') is not None else None
+    #print('Digest Text :'+ str(digesttext))
+    
+    bill_data.append({
+            "Bill ID": Id,
             "Title": title,
             "Session Year": session_year,
-            "Measure Type": measure_type,
-            "Subject": subject,
-            "Source File": filename
+            "Source File": filename,
+            "Digesgt Text": digesttext
         })
-        """
-#df = pd.DataFrame(bill_data)
-#print(df)
+
+df=pd.DataFrame.from_dict(bill_data)
+print(df)
+
+output =os.makedirs(today+'CSV',exist_ok=True)
+csv_name=today+'/bill_data.csv'
+print(csv_name)
+df.to_csv(csv_name,index=False)
